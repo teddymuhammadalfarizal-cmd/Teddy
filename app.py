a20c7a592e7b0738ea476d650056d772a81d2225
teddymuhammadalfarizal-cmd <teddymuhammadalfarizal@gmail.com>
2026-07-10 22:09:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
+from PIL import Image
 
 st.title("AI Face Swap Abi")
-st.write("Wilujeng sumping di aplikasi AI buatan abi!")
+st.write("Upload foto anjeun di dieu:")
 
-user_input = st.text_input("Tulis nami anjeun:")
-if user_input:
-    st.write(f"Halo {user_input}, 
+uploaded_file = st.file_uploader("Pilih gambar...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Foto nu di-upload')
+    st.write("Gambar tos siap!")
